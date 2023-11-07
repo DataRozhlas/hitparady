759c4ce36a1ea0f3dffdf6fb848486a800574181
Michal Kašpárek <michal.kasparek@gmail.com>
2023-11-07 19:32:35 +0100

    1. várka grafů

---
+++ src/graf.py
@@ -0,0 +1,173 @@
+def graf(
+    carovy=[],
+    sloupcovy=[],
+    vodorovny=[],
+    procenta=[],
+    skryte=[],
+    barvy=[],
+    histogram=False,
+    max_procenta=100,
+    target="",
+    titulek="",
+    podtitulek="",
+    osay=" ",
+    osay2=" ",
+    osaymin=None,
+    osaymax=None,
+    kredity="",
+    zaokrouhleni=1,
+    prvni=True,
+    skladany=False,
+    naopak=False,
+    vzhurunohama=False
+):
+    import os
+    import pandas as pd
+    from highcharts_core.chart import Chart
+    from highcharts_core.options.series.area import LineSeries
+    from highcharts_core.options.series.bar import ColumnSeries
+    from highcharts_core.options.series.bar import BarSeries
+    from highcharts_core.options.series.histogram import HistogramSeries
+    from highcharts_core.options.legend import Legend
+    from highcharts_core.options.title import Title
+    from highcharts_core.options.subtitle import Subtitle
+    from highcharts_core.options.credits import Credits
+
+    nastaveni = {}
+
+    if prvni:
+        zdrojaky = f"""<script src="https://code.highcharts.com/highcharts.js"></script><style type="text/css">text{{font-family:"Asap"!important}}.paragraph{{font-family:"Noticia text"!important}}.href{{color:#666;fill:#666}}.highcharts-title{{font-family:"Noticia text"!important;font-weight:700!important;text-align:left!important;left:10px!important}}.highcharts-subtitle{{text-align:left!important;font-size:.95rem!important;left:10px!important;font-family:"Asap"!important}}.highcharts-data-labels text{{font-size:.85rem!important}}.highcharts-axis-labels text{{font-size:.85rem!important}}text.highcharts-plot-line-label{{font-size:.85rem!important;fill:#666}}text.highcharts-plot-band-label{{font-size:.85rem!important;fill:#666}}text.highcharts-credits{{font-size:.75rem!important}}.highcharts-tooltip span{{font-family:"Asap"!important}}.axis-label-on-tick{{fill:#aaa;color:#aaa}}.mock-empty-line{{fill:#fff;color:#fff}}</style>"""
+    else:
+        zdrojaky = ""
+
+    pred = f"""{zdrojaky}
+        <figure id="{target}">
+        <div id="container"></div>
+        </figure>
+        <script>"""
+
+    if len(carovy) > 0:
+        categories = carovy[0].index.to_list()
+    if len(sloupcovy) > 0:
+        categories = sloupcovy[0].index.to_list()
+    if len(vodorovny) > 0:
+        categories = vodorovny[0].index.to_list()
+
+    categories = [str(x) for x in categories]
+
+    nastaveni["xAxis"] = {"categories": categories, "min": 0}
+    nastaveni["yAxis"] = [{"title": {"text": osay}, "reversed": vzhurunohama, "max": osaymax, "min": osaymin}]
+
+    if skladany:
+        if len(sloupcovy) > 0:
+            nastaveni["plotOptions"] = {"column": {"stacking": "normal"}}
+        if len(vodorovny) > 0:
+            nastaveni["plotOptions"] = {"bar": {"stacking": "normal"}}
+    if histogram:
+        nastaveni["plotOptions"] = {
+            "column": {
+                "pointPadding": 0,
+                "borderWidth": 0,
+                "groupPadding": 0,
+                "shadow": False,
+            }
+        }
+
+    if len(procenta) > 0:
+        osa_procent = {
+            "title": {"text": osay2},
+            "max": max_procenta,
+            "min": 0,
+            "labels": {"format": "{value} %"},
+        }
+
+        if len(procenta) != len(carovy) + len(sloupcovy):
+            osa_procent["opposite"] = True
+            druha_osa = 1
+            nastaveni["yAxis"].append(osa_procent)
+            nastaveni["alignTicks"] = False
+        if len(procenta) == len(carovy) + len(sloupcovy):
+            nastaveni["yAxis"] = [osa_procent]
+            druha_osa = 0
+
+    my_chart = Chart(container=target, options=nastaveni)
+
+    procenta = [p.name for p in procenta]
+    skryte = [s.name for s in skryte]
+
+    def vykresleni(serie, typ):
+        for s in serie:
+            popisek = s.name
+
+            if s.name in skryte:
+                viditelnost = False
+            else:
+                viditelnost = True
+
+            if s.name in procenta:
+                s = [round(x * 100, zaokrouhleni) for x in s.fillna(0).to_list()]
+                my_chart.add_series(
+                    typ(
+                        data=s,
+                        visible=viditelnost,
+                        name=popisek,
+                        y_axis=druha_osa,
+                        tooltip={"valueSuffix": " %"},
+                    )
+                )
+            else:
+                my_chart.add_series(
+                    typ(
+                        data=s.fillna(0).to_list(),
+                        visible=viditelnost,
+                        name=popisek,
+                        y_axis=0,
+                    )
+                )
+
+    if len(sloupcovy) > 0:
+        vykresleni(sloupcovy, ColumnSeries)
+    if len(carovy) > 0:
+        vykresleni(carovy, LineSeries)
+    if len(vodorovny) > 0:
+        vykresleni(vodorovny, BarSeries)
+
+    if len(barvy) > 0:
+        my_chart.options.colors = barvy
+    else:
+        my_chart.options.colors = colors = [
+            "#b2e061",  ## světle zelená (light green)
+            "#7eb0d5",  ## světle modrá (light blue)
+            "#fd7f6f",  ## světle červená (light red)
+            "#bd7ebe",  ## světle fialová (light purple)
+            "#ffb55a",  ## oranžová (orange)
+            "#ffee65",  ## žlutá (yellow)
+            "#beb9db",  ## levandulová (lavender)
+            "#fdcce5",  ## skoro černá
+            "#8bd3c7",  ## světle tyrkysová (light turquoise)
+        ]
+
+    if naopak:
+        my_chart.options.legend = Legend(reversed=True)
+
+    my_chart.options.title = Title(text=titulek, align="left", margin=30)
+
+    if len(podtitulek) > 0:
+        my_chart.options.subtitle = Subtitle(text=podtitulek, align="left")
+
+    my_chart.options.credits = Credits(text=kredity[0], enabled=True, href=kredity[1])
+
+    as_js_literal = my_chart.to_js_literal()
+
+    code = f"<html><head><title>{titulek}</title></head><body>{pred}{as_js_literal}</script></body></html>"
+
+    if not os.path.exists("grafy"):
+        os.mkdir("grafy")
+
+    with open(os.path.join("grafy", target + ".html"), "w+") as f:
+        f.write(code)
+
+    with open(os.path.join("grafy", target + ".txt"), "w+") as f:
+        f.write(f"{pred}{as_js_literal}</script>")
+
+        print("Graf uložen.")
---
+++ src/tabulka.py
@@ -0,0 +1,78 @@
+def tabulka(
+    frame,
+    titulek="",
+    podtitulek="",
+    vysvetlivka="",  # zpětná kompatibilita
+    bez_tecky=[],
+    na_procenta=[],
+    poradi=False,
+    bez_zavorek=True,
+):
+    import pandas as pd
+
+    if podtitulek != "":
+        vysvetlivka = podtitulek
+
+    df_tabulka = frame.copy()
+
+    sloupcu = len(df_tabulka.columns)
+    
+    def cela(x):
+        try:
+            x = int(x)
+        except:
+            x = "–"
+        return x
+
+    import re
+
+    if poradi:
+        sloupce = df_tabulka.columns.tolist()
+        df_tabulka = df_tabulka.reset_index(drop=True)
+        df_tabulka[" "] = pd.to_numeric(df_tabulka.index)
+        df_tabulka[" "] = df_tabulka[" "].apply(lambda x: str(x + 1) + ".")
+        nove_sloupce = [" "]
+        for s in sloupce:
+            nove_sloupce.append(s)
+        df_tabulka = df_tabulka[nove_sloupce]
+
+    if len(bez_tecky) > 0:
+        for b in bez_tecky:
+            df_tabulka[b] = df_tabulka[b].apply(lambda x: cela(x))
+
+    if len(na_procenta) > 0:
+        for p in na_procenta:
+            df_tabulka[p] = df_tabulka[p].apply(lambda x: "{:.1%}".format(x))
+            df_tabulka[p] = (
+                df_tabulka[p]
+                .astype("string")
+                .apply(lambda x: x.replace(".", ",").replace("%", " %"))
+            )
+
+    df_tabulka = re.sub("\\n\s*", "", df_tabulka.to_html(index=False))
+    df_tabulka = (
+        df_tabulka.replace("<th>", '<th class="text-nowrap">')
+        .replace("<tbody>", '<tbody class="text-sm">')
+        .replace("border=\"1\" ", "")
+        .replace(
+            'class=\"dataframe\"',
+            'class=\"dataframe table table--responsive table--w100p table--striped-red table--plain\"',
+        )
+        .replace(" , ", ", ")
+    )
+
+    if len(titulek) > 0:
+        df_tabulka = df_tabulka.replace(
+            "<thead", f"<caption>{titulek}</caption><thead")
+
+    if len(vysvetlivka) > 0:
+        df_tabulka = df_tabulka.replace(
+            "</tbody>",
+            f'</tbody><tfoot><tr style="text-align: center;"><td colspan={sloupcu}>{vysvetlivka}</td></tr></tfoot>',
+        )
+
+    if bez_zavorek:
+        df_tabulka = re.sub("\([\d]*\)", "", df_tabulka)
+        df_tabulka = df_tabulka.replace(" </td>", "</td>")
+        
+    return (df_tabulka)
